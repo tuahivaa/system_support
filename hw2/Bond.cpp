31222d82c95548e761f224f7f0bc8e120cf4ea85
Guillaume PC <guillaume.tuahivaatetonohiti.com>
2023-09-11 16:53:41 -0400

    added comment

--- hw2/Bond.cpp
+++ hw2/Bond.cpp
@@ -1,10 +1,13 @@
 #include "Bond.h"
 
+// default
 Bond::Bond() : expirationDate("01/01/0000"), frequencyOfPayments(0.0), couponRate(0.0) {}
+
 
 Bond::Bond(const std::string& expirationDate, double frequencyOfPayments, double couponRate)
     : expirationDate(expirationDate), frequencyOfPayments(frequencyOfPayments), couponRate(couponRate) {}
 
+// copy constructor
 Bond::Bond(const Bond& other)
     : expirationDate(other.expirationDate), frequencyOfPayments(other.frequencyOfPayments), couponRate(other.couponRate) {}
 
