50e59e95e5756556c1919eba36f465f002256667
Tuahivaa Laptop <guillaume.tuahivaatetonohiti@mail.com>
2023-09-13 13:20:37 -0400

    added data

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -3,13 +3,17 @@
 #include "Bond.h"
 #include "Bond.cpp"
 
+using namespace std;
+
 int main() {
 
     // Create a new semi-annually compounded bond
     Bond customBond("11/19/2035", 0.5, 0.07);
 
     // Print its characteristics using the ToString() function
-    std::cout << "Custom Bond: " << customBond.ToString() << std::endl;
+    cout << "Custom Bond: " << customBond.ToString() << endl;
+
+    cout << customBond.PriceBond(0.4,1);
 
     return 0;
 }
